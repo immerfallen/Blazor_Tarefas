d4053966d998f34317ae059ab8f55cce84c9fa80
immerfallen <marodemelo10@gmail.com>
2022-05-31 11:09:08 -0300

    Passando tipos complexos por cascata

---
+++ Blazor_Tarefas/Utils/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor_Tarefas.Utils
+{
+    public class Config
+    {
+        public string CorFundo { get; set; } = "cyan";
+        public string TamanhoFonte { get; set; } = "120px";
+    }
+}
