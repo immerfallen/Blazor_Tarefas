1dcf5accda2d6c5e2e0e21679f31efb2b5840a06
immerfallen <marodemelo10@gmail.com>
2022-06-13 15:45:29 -0300

    Usando ValidationSummary

--- Blazor_Tarefas/Models/Usuario.cs
+++ Blazor_Tarefas/Models/Usuario.cs
@@ -6,13 +6,15 @@ namespace Blazor_Tarefas.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage="O nome deve ser inserido.")]
+        [StringLength(100, ErrorMessage="O nome não pode exceder 100 caracteres.")]
         public string Nome { get; set; }
 
-        [EmailAddress]
+        [Required(ErrorMessage = "O e-mail deve ser inserido.")]
+        [EmailAddress(ErrorMessage = "Insira um e-mail válido.")]
         public string Email { get; set; }
 
-        [Range(18,80, ErrorMessage ="A idade deve estar entre 18 anos e 80 anos")]
+        [Range(18,80, ErrorMessage ="A idade deve estar entre 18 anos e 80 anos.")]
         public int Idade { get; set; }
     }
 }
