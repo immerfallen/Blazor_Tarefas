807c23d07991dee69dc9e217562fedc18417097d
immerfallen <marodemelo10@gmail.com>
2022-06-13 15:02:28 -0300

    Criando primeiro formulario

---
+++ Blazor_Tarefas/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazor_Tarefas.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public int Idade { get; set; }
+    }
+}
